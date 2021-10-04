5446e28703b282be2b020d4f3a53f76bc58839a5
William Feistel <lee@feistel.net>
2021-10-03 19:13:17 -0500

    refactored the code to break the form handler event apart from creating the task element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
  
-  var taskNameInput = document.querySelector("input[name='task-name']");    
-  var taskTypeInput = document.querySelector("select[name='task-type']");
+  var taskDataObj = {
+    name : document.querySelector("input[name='task-name']").value,
+    type : document.querySelector("select[name='task-type']").value
+  };
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
   var listItemEl = document.createElement("li");
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   listItemEl.className = "task-item";
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput.value + "</h3><span class='task-type'>" + taskTypeInput.value + "</span>";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
   tasksToDoEl.appendChild(listItemEl);
-};
+}
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
